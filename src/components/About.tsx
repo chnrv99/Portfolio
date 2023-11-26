5d85d793aebd8ffad30751e06c0fddc6c866947b
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-11-26 17:14:08 +0530

    added rounded to photos

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ export default function About({ }: Props) {
     return (
         <div className="container flex lg:flex-row sm:flex-col text-white">
             <div className="container ">
-                <Image src={image1} width={400} height={400} alt="Picture of the author" />
+                <Image src={image1} width={400} height={400} className='rounded-md justify-center' alt="Picture of the author" />
             </div>
             <div className="container  ">
                 <div className="container flex-col  basis-1/3">
@@ -50,15 +50,17 @@ export default function About({ }: Props) {
                     </div>
                 </div>
 
+                <h1 className='mt-6'>Title 1</h1>
+                <p className=' basis-1/3 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tempora omnis ab aliquid at corrupti minima laboriosam quod possimus culpa repellendus earum sequi ipsa pariatur odit, molestias cumque est sint voluptatibus ipsam consequuntur rem temporibus ducimus laborum? Iusto, facere consequatur!</p>
 
-                <p className=' basis-1/3 mt-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tempora omnis ab aliquid at corrupti minima laboriosam quod possimus culpa repellendus earum sequi ipsa pariatur odit, molestias cumque est sint voluptatibus ipsam consequuntur rem temporibus ducimus laborum? Iusto, facere consequatur!</p>
+                <h1 className='mt-6'>Title 1</h1>
+                <p className="basis-1/6 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, iste suscipit. Autem magnam est laudantium! Molestias nostrum nisi quia possimus eum blanditiis dolor aspernatur expedita ipsum. Voluptates sint impedit, debitis accusantium nobis, dolorum natus ab aliquid alias laboriosam unde aliquam!</p>
 
-                <p className="basis-1/6 mt-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, iste suscipit. Autem magnam est laudantium! Molestias nostrum nisi quia possimus eum blanditiis dolor aspernatur expedita ipsum. Voluptates sint impedit, debitis accusantium nobis, dolorum natus ab aliquid alias laboriosam unde aliquam!</p>
+                <h1 className='mt-6'>Title 1</h1>
+                <p className="basis-1/6 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, iste suscipit. Autem magnam est laudantium! Molestias nostrum nisi quia possimus eum blanditiis dolor aspernatur expedita ipsum. Voluptates sint impedit, debitis accusantium nobis, dolorum natus ab aliquid alias laboriosam unde aliquam!</p>
 
-                <p className="basis-1/6 mt-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, iste suscipit. Autem magnam est laudantium! Molestias nostrum nisi quia possimus eum blanditiis dolor aspernatur expedita ipsum. Voluptates sint impedit, debitis accusantium nobis, dolorum natus ab aliquid alias laboriosam unde aliquam!</p>
-
-
-                <p className="basis-1/6 mt-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, iste suscipit. Autem magnam est laudantium! Molestias nostrum nisi quia possimus eum blanditiis dolor aspernatur expedita ipsum. Voluptates sint impedit, debitis accusantium nobis, dolorum natus ab aliquid alias laboriosam unde aliquam!</p>
+                <h1 className='mt-6'>Title 1</h1>
+                <p className="basis-1/6 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, iste suscipit. Autem magnam est laudantium! Molestias nostrum nisi quia possimus eum blanditiis dolor aspernatur expedita ipsum. Voluptates sint impedit, debitis accusantium nobis, dolorum natus ab aliquid alias laboriosam unde aliquam!</p>
 
                 
 
