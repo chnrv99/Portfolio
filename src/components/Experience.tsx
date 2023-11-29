0ed5c4a19465a582b51b2ecb0c86775b571f2abf
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-11-29 19:44:58 +0530

    experience-done

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -34,16 +34,16 @@ type CardProps = React.ComponentProps<typeof Card>
 export default function Experience({ className, ...props }: CardProps) {
     return (
         <>
-            <div className=" flex flex-row items-center">
-                <div className="container flex-auto ">
+            <div className="lg:flex lg:flex-row sm:flex sm:flex-col items-center ">
+                <div className="lg:container lg:flex-auto ">
                     <Card className={cn("w-auto h-auto hover:bg-slate-900 hover:text-slate-200 transition-colors", className)} {...props}>
                         <CardHeader>
                             <div className="flex justify-between">
                                 <CardTitle className="flex bg-gradient-to-r from-blue-900 to-blue-300 text-transparent bg-clip-text">Application Developer</CardTitle>
-                                <CardDescription className="flex">
-                                    <Badge className="mr-2  bg-red-200">MERN Stack</Badge>
-                                    <Badge className="mr-2 bg-blue-200">AWS</Badge>
-                                    <Badge className="mr-2  bg-violet-200">git</Badge>
+                                <CardDescription className="lg:flex lg:flex-row sm:flex-col md:flex-col">
+                                    <Badge className="lg:mr-2  bg-red-200">MERN</Badge>
+                                    <Badge className="lg:mr-2 bg-blue-200">AWS</Badge>
+                                    <Badge className="lg:mr-2  bg-violet-200">git</Badge>
                                 </CardDescription>
 
                             </div>
@@ -61,9 +61,7 @@ export default function Experience({ className, ...props }: CardProps) {
                                         <p className="text-xl font-medium leading-none text-blue-100">
                                             Crackube
                                         </p>
-                                        <p className="text-sm text-muted-foreground">
-
-                                        </p>
+                                        
                                     </div>
                                 </div>
 
@@ -99,14 +97,15 @@ export default function Experience({ className, ...props }: CardProps) {
                             August 2023 - November 2023
                         </CardFooter>
                     </Card>
+                    <br />
                 </div>
-                <div className="container ">
-                    <div className="container flex-auto">
+                <div className="lg:container md:my-2">
+                    <div className="lg:container lg:flex-auto sm:flex-col md:flex-col md:my-2">
                         <Card className={cn("w-auto h-auto  hover:bg-slate-900 hover:text-slate-200 transition-colors", className)} {...props}>
                             <CardHeader>
                                 <div className="flex justify-between">
                                     <CardTitle className="flex bg-gradient-to-r from-blue-900 to-blue-300 text-transparent bg-clip-text">Student Ambassador</CardTitle>
-                                    <CardDescription className="flex">
+                                    <CardDescription className="lg:flex lg:flex-row sm:flex-col md:flex-col">
                                         <Badge className="mr-2 bg-green-200">Azure</Badge>
                                         <Badge className="mr-2 bg-slate-200">Open source</Badge>
                                         <Badge className="mr-2 bg-orange-200">Flask</Badge>
@@ -124,13 +123,11 @@ export default function Experience({ className, ...props }: CardProps) {
                                         className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
                                     >
                                         <span className="flex h-2 w-2 translate-y-1 rounded-full" />
-                                        <div className="space-y-1">
+                                        <div className="">
                                             <p className="text-xl font-medium leading-none text-blue-100">
                                                 Microsoft
                                             </p>
-                                            <p className="text-sm text-muted-foreground">
-
-                                            </p>
+                                            
                                         </div>
                                     </div>
 
@@ -168,16 +165,16 @@ export default function Experience({ className, ...props }: CardProps) {
                         </Card>
                     </div>
                     <br />
-                    <div className="container flex-auto">
+                    <div className="lg:container lg:flex-auto md:flex-col sm:flex-col md:my-2">
                         <Card className={cn("w-auto h-auto  hover:bg-slate-900 hover:text-slate-200 transition-colors", className)} {...props}>
-                            <CardHeader>
+                            <CardHeader className="">
                                 <div className="flex justify-between">
                                     <CardTitle className="flex bg-gradient-to-r from-blue-900 to-blue-300 text-transparent bg-clip-text">Interim Dev Lead</CardTitle>
-                                    <CardDescription className="flex">
-                                        <Badge className="mr-2 bg-amber-200">Next.js</Badge>
-                                        <Badge className="mr-2 bg-lime-200">MERN</Badge>
-                                        <Badge className="mr-2 bg-emerald-200">Azure</Badge>
-                                        <Badge className="mr-2 bg-indigo-200">Hacking</Badge>
+                                    <CardDescription className="sm:hidden md:hidden lg:flex lg:flex-row">
+                                        <Badge className="lg:mr-2  bg-amber-200">Next.js</Badge>
+                                        <Badge className="lg:mr-2  bg-lime-200">MERN</Badge>
+                                        <Badge className="lg:mr-2  bg-emerald-200">Azure</Badge>
+                                        <Badge className="lg:mr-2  bg-indigo-200">Hacking</Badge>
                                     </CardDescription>
 
                                 </div>
@@ -195,9 +192,7 @@ export default function Experience({ className, ...props }: CardProps) {
                                             <p className="text-xl font-medium leading-none text-blue-100">
                                                 CYSCOM VIT
                                             </p>
-                                            <p className="text-sm text-muted-foreground">
-
-                                            </p>
+                                            
                                         </div>
                                     </div>
 
