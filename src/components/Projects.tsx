c1454082108da299e0254aac4b13deddc8692760
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-11-29 15:14:52 +0530

    did something

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -39,150 +39,14 @@ export default function Projects({ className, ...props }: CardProps) {
             <div className=" flex flex-row items-center">
 
 
-                <div className="container ">
+                <div className="mr-6 ">
                     <div className="container flex-auto">
                         <Card className={cn("w-auto h-auto", className)} {...props}>
                             <CardHeader>
                                 <div className="flex justify-between">
                                     <CardTitle className="flex">Notifications</CardTitle>
                                     <CardDescription className="flex">
-                                        <Badge className="mr-2">New</Badge>
-                                        <Badge className="mr-2">New</Badge>
-                                        <Badge className="mr-2">New</Badge>
-                                        <Badge className="mr-2">New</Badge>
-                                    </CardDescription>
 
-                                </div>
-                            </CardHeader>
-                            <CardContent className="grid gap-4">
-
-                                <div>
-                                    {notifications.map((notification, index) => (
-                                        <div
-                                            key={index}
-                                            className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-                                        >
-                                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                                            <div className="space-y-1">
-                                                <p className="text-sm font-medium leading-none">
-                                                    {notification.title}
-                                                </p>
-                                                <p className="text-sm text-muted-foreground">
-                                                    <Badge className="mr-2">New</Badge>
-                                                    <Badge className="mr-2">New</Badge>
-                                                    <Badge className="mr-2">New</Badge>
-                                                    <Badge className="mr-2">New</Badge>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </CardContent>
-                            <CardFooter>
-
-                            </CardFooter>
-                        </Card>
-                    </div>
-                    <br />
-
-                    <div className="flex">
-
-                        <div className="container flex-auto ">
-                            <Card className={cn("w-auto h-auto", className)} {...props}>
-                                <CardHeader>
-                                    <div className="flex justify-between">
-                                        <CardTitle className="flex">Notifications</CardTitle>
-                                        <CardDescription className="flex">
-                                            <Badge className="mr-2">New</Badge>
-                                            <Badge className="mr-2">New</Badge>
-                                            <Badge className="mr-2">New</Badge>
-                                            <Badge className="mr-2">New</Badge>
-                                        </CardDescription>
-
-                                    </div>
-                                </CardHeader>
-                                <CardContent className="grid gap-4">
-
-                                    <div>
-                                        {notifications.map((notification, index) => (
-                                            <div
-                                                key={index}
-                                                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-                                            >
-                                                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                                                <div className="space-y-1">
-                                                    <p className="text-sm font-medium leading-none">
-                                                        {notification.title}
-                                                    </p>
-                                                    <p className="text-sm text-muted-foreground">
-                                                        {notification.description}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </CardContent>
-                                <CardFooter>
-
-                                </CardFooter>
-                            </Card>
-                        </div>
-                        <br />
-
-                        <div className="container flex-auto ">
-                            <Card className={cn("w-auto h-auto", className)} {...props}>
-                                <CardHeader>
-                                    <div className="flex justify-between">
-                                        <CardTitle className="flex">Notifications</CardTitle>
-                                        <CardDescription className="flex">
-                                            <Badge className="mr-2">New</Badge>
-                                            <Badge className="mr-2">New</Badge>
-                                            <Badge className="mr-2">New</Badge>
-                                            <Badge className="mr-2">New</Badge>
-                                        </CardDescription>
-
-                                    </div>
-                                </CardHeader>
-                                <CardContent className="grid gap-4">
-
-                                    <div>
-                                        {notifications.map((notification, index) => (
-                                            <div
-                                                key={index}
-                                                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-                                            >
-                                                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                                                <div className="space-y-1">
-                                                    <p className="text-sm font-medium leading-none">
-                                                        {notification.title}
-                                                    </p>
-                                                    <p className="text-sm text-muted-foreground">
-                                                        {notification.description}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </CardContent>
-                                <CardFooter>
-
-                                </CardFooter>
-                            </Card>
-                        </div>
-                    </div>
-
-                    <br />
-
-                    <div className="container flex-auto ">
-                        <Card className={cn("w-auto h-auto", className)} {...props}>
-                            <CardHeader>
-                                <div className="flex justify-between">
-                                    <CardTitle className="flex">Notifications</CardTitle>
-                                    <CardDescription className="flex">
-                                        <Badge className="mr-2">New</Badge>
-                                        <Badge className="mr-2">New</Badge>
-                                        <Badge className="mr-2">New</Badge>
-                                        <Badge className="mr-2">New</Badge>
                                     </CardDescription>
 
                                 </div>
@@ -206,6 +70,151 @@ export default function Projects({ className, ...props }: CardProps) {
                                             </div>
                                         </div>
                                     ))}
+                                    <p className="text-sm text-muted-foreground">
+                                        <Badge className="mr-2">New</Badge>
+                                        <Badge className="mr-2">New</Badge>
+                                        <Badge className="mr-2">New</Badge>
+                                        <Badge className="mr-2">New</Badge>
+                                    </p>
+                                </div>
+                            </CardContent>
+                            <CardFooter>
+
+                            </CardFooter>
+                        </Card>
+                    </div>
+                    <br />
+
+                    <div className="flex">
+
+                        <div className="container flex-auto ">
+                            <Card className={cn("w-auto h-auto", className)} {...props}>
+                                <CardHeader>
+                                    <div className="flex justify-between">
+                                        <CardTitle className="flex">Notifications</CardTitle>
+                                        <CardDescription className="flex">
+
+                                        </CardDescription>
+
+                                    </div>
+                                </CardHeader>
+                                <CardContent className="grid gap-4">
+
+                                    <div>
+                                        {notifications.map((notification, index) => (
+                                            <div
+                                                key={index}
+                                                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                                            >
+                                                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                                                <div className="space-y-1">
+                                                    <p className="text-sm font-medium leading-none">
+                                                        {notification.title}
+                                                    </p>
+                                                    <p className="text-sm text-muted-foreground">
+                                                        {notification.description}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        ))}
+                                        <p className="text-sm text-muted-foreground">
+                                            <Badge className="mr-2">New</Badge>
+                                            <Badge className="mr-2">New</Badge>
+                                            <Badge className="mr-2">New</Badge>
+                                            <Badge className="mr-2">New</Badge>
+                                        </p>
+                                    </div>
+                                </CardContent>
+                                <CardFooter>
+
+                                </CardFooter>
+                            </Card>
+                        </div>
+                        <br />
+
+                        <div className="container flex-auto ">
+                            <Card className={cn("w-auto h-auto", className)} {...props}>
+                                <CardHeader>
+                                    <div className="flex justify-between">
+                                        <CardTitle className="flex">Notifications</CardTitle>
+                                        <CardDescription className="flex">
+
+                                        </CardDescription>
+
+                                    </div>
+                                </CardHeader>
+                                <CardContent className="grid gap-4">
+
+                                    <div>
+                                        {notifications.map((notification, index) => (
+                                            <div
+                                                key={index}
+                                                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                                            >
+                                                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                                                <div className="space-y-1">
+                                                    <p className="text-sm font-medium leading-none">
+                                                        {notification.title}
+                                                    </p>
+                                                    <p className="text-sm text-muted-foreground">
+                                                        {notification.description}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        ))}
+                                        <p className="text-sm text-muted-foreground">
+                                            <Badge className="mr-2">New</Badge>
+                                            <Badge className="mr-2">New</Badge>
+                                            <Badge className="mr-2">New</Badge>
+                                            <Badge className="mr-2">New</Badge>
+                                        </p>
+                                    </div>
+                                </CardContent>
+                                <CardFooter>
+
+                                </CardFooter>
+                            </Card>
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div className="container flex-auto ">
+                        <Card className={cn("w-auto h-auto", className)} {...props}>
+                            <CardHeader>
+                                <div className="flex justify-between">
+                                    <CardTitle className="flex">Notifications</CardTitle>
+                                    <CardDescription className="flex">
+
+                                    </CardDescription>
+
+                                </div>
+                            </CardHeader>
+                            <CardContent className="grid gap-4">
+
+                                <div>
+                                    {notifications.map((notification, index) => (
+                                        <div
+                                            key={index}
+                                            className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                                        >
+                                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                                            <div className="space-y-1">
+                                                <p className="text-sm font-medium leading-none">
+                                                    {notification.title}
+                                                </p>
+                                                <p className="text-sm text-muted-foreground">
+                                                    {notification.description}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    ))}
+                                    <p className="text-sm text-muted-foreground">
+                                        <Badge className="mr-2">New</Badge>
+                                        <Badge className="mr-2">New</Badge>
+                                        <Badge className="mr-2">New</Badge>
+                                        <Badge className="mr-2">New</Badge>
+                                    </p>
                                 </div>
                             </CardContent>
                             <CardFooter>
